b658354a60e76dcd3f50e3c839d7c2e0a5227097
Lucas Ji <lucasji@qq.com>
2021-12-21 17:22:27 +0800

    indentline.lua

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -55,6 +55,7 @@ return packer.startup(function(use)
   use "ahmedkhalf/project.nvim"
   use "lewis6991/impatient.nvim"
   use "tpope/vim-surround"
+  use "lukas-reineke/indent-blankline.nvim"
 
   -- Colorschemes
   -- use "lunarvim/colorschemes" -- A bunch of colorschemes you can try out
