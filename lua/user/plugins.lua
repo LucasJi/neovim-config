c5a99f2a3cadf0894b5c8ea6d21b6b8818f25d23
Lucas Ji <lucasji@qq.com>
2021-12-21 17:25:23 +0800

    alpha.lua

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -56,6 +56,8 @@ return packer.startup(function(use)
   use "lewis6991/impatient.nvim"
   use "tpope/vim-surround"
   use "lukas-reineke/indent-blankline.nvim"
+  use 'goolord/alpha-nvim'
+  use "antoinemadec/FixCursorHold.nvim" -- This is needed to fix lsp doc highlight
 
   -- Colorschemes
   -- use "lunarvim/colorschemes" -- A bunch of colorschemes you can try out
