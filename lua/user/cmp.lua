c4547eb2d4cf2f3daa92cb11c7b597fdb1b7133f
Lucas Ji <lucasji@qq.com>
2022-05-12 23:05:25 +0800

    Update nvim-cmp plugin config

--- lua/user/cmp.lua
+++ lua/user/cmp.lua
@@ -117,9 +117,9 @@ cmp.setup({
 		behavior = cmp.ConfirmBehavior.Replace,
 		select = false,
 	},
-	documentation = {
-		border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
-	},
+  window = {
+    documentation = cmp.config.window.bordered(),
+  },
 	experimental = {
 		ghost_text = false,
 		native_menu = false,
